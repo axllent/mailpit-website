2030d4ac1b4cb35539b1a1063ef511c7dca49cae
Ralph Slooten <axllent@gmail.com>
2025-06-21 09:15:20 +1200

    Apply linting and JavaScript fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-import "./vue"
-import "bootstrap"
+import "./vue";
+import "bootstrap";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
 	// open external links in new window
-	const l = document.getElementsByTagName("a")
+	const l = document.getElementsByTagName("a");
 	const len = l.length;
-	for (i = 0; i < len; ++i) {
+	for (let i = 0; i < len; ++i) {
 		const a = l[i];
-		if (a.href && !a.target && (a.href.indexOf(location.host) == -1 && a.href.match(/^https?\:\/\//i))) {
+		if (a.href && !a.target && a.href.indexOf(location.host) === -1 && a.href.match(/^https?:\/\//i)) {
 			a.target = "_blank";
-			a.rel += a.rel ? !a.rel.match(/noopener/i) ? " noopener" : '' : 'noopener';
+			a.rel += a.rel ? (!a.rel.match(/noopener/i) ? " noopener" : "") : "noopener";
 		}
 	}
-})
+});
